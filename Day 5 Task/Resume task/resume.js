2e571a8870b65dc665a03b8506f66ca0b1667149
Baburajan03 <130633153+Baburajan03@users.noreply.github.com>
2023-05-18 03:41:24 +0530

    Add files via upload

---
+++ Day 5 Task/Resume task/resume.js
@@ -0,0 +1,33 @@
+let resume =[
+    {
+    "Name"   : "Babu Rajan",
+    "Mobile" : "9597331789",
+    "Email"  : "baburajan.r03@gmail.com",
+    "Technical_skills" : ["Ms Office", "html", "mangoDB", "React"],
+    "Academic_details" : [{
+       "Course"          : "Bachelor of Engineering",
+       "Institute"       : "Vel Tech Engineering College",
+       "Year_of_passing" : "Nov 2020",
+        "CGPA"            : "7",
+    },
+        
+    {    
+        "Course"          : "HSC",
+        "Institute"       : "Lakshmi Garden School",
+        "Year_of_passing" : "Apr 2015",
+        "Percentage"       : "80%",
+    },
+    ],
+        "Work_Experience" : [{
+        "company_name"    : "KS Builders",
+        "designation"     : "Engineer",
+        "Year_of_Exp"     : "3 Years",
+    }],
+    
+    "Languages_known" : ["Tamil", "English", "Hindi"],
+}]
+for(i=0;i<resume.length;i++)
+
+console.log(resume[i])
+
+
